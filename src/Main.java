d705c3fb3fc96485637aeac5f88de53413936b1d
eyal-kaufman <59081215+eyal-kaufman@users.noreply.github.com>
2021-01-03 08:10:46 +0200

    reading variable lines

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
 
 
 	public boolean checkName(String name) {
-		Pattern pattern = Pattern.compile("_+[a-zA-Z0-9]+|_*[a-zA-Z]+\\w");
+		Pattern pattern = Pattern.compile("_+[a-zA-Z0-9]+|_*[a-zA-Z]\\w*");
 		return pattern.matcher(name).matches();
 	}
 
@@ -18,14 +18,14 @@ public class Main {
 		return pattern.matcher(var).matches();
 	}
 	public boolean variableCheck(String line) {
-		String[] splitLine= line.trim().split(" ", 1);
+		String[] splitLine= line.trim().split(" ", 3);
 		//			if the length is smaller than 2, then the variables have been declared without a type.
 		//			or the type is not valid, or the line doesn't end with ; sign.
 		if (splitLine.length <2 || !this.validType(splitLine[0])
 			|| !this.validSemicolonSuffix(splitLine[splitLine.length-1])) {
 			return false;
 		}
-		Pattern pattern = Pattern.compile("[^,;]+ *=* *[^,;]+ *[,;]");
+		Pattern pattern = Pattern.compile("[^,;]+? *=* *[^,;]+ *[,;]");
 		Matcher matcher = pattern.matcher(splitLine[1]);
 		while (matcher.find()) {
 			splitLine[1].substring(matcher.start(),matcher.end());
@@ -36,6 +36,18 @@ public class Main {
 	}
 
 	public static void main(String[] args) {
+		int a = 0;
+		String[] splitLine= "a = 3;".trim().split(" *\\w+ *[=,;] *.*",2)[0].trim().split(" ",1);
+		String[] splitDeclaration = "b ".split(" *=[^\"]", 2);;
+//		Pattern pattern = Pattern.compile("[,;]");
+//		Matcher matcher = pattern.matcher("a, b = b, int b=0, final int b,c;");
+//		while (matcher.find()){
+//			System.out.println("a, b = b, int b=0, final int b,c;".substring(matcher.start(),matcher.end()));
+//		}
+		System.out.println(splitDeclaration.length);
+//		for (String st : splitDeclaration) {
+//			System.out.println(st.trim());
+//		}
 		Main m = new Main();
 		System.out.println(m.checkName("0"));
 	}
--- src/VariableFactory.java
+++ src/VariableFactory.java
@@ -1,3 +1,94 @@
-public class VariableFactory {
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+public class VariableFactory {
+	private final Pattern validNamePattern = Pattern.compile("_+[a-zA-Z0-9]+|_*[a-zA-Z]\\w*");
+	private Boolean isFinal;
+	private String type;
+
+	public boolean checkName(String name) {
+		return this.validNamePattern.matcher(name).matches();
+	}
+
+	/**
+	 * this method get line of variable declaration that is finished with , or ;
+	 * and create from the given line the appropriate variables
+	 * @param line
+	 */
+	public void parseDeclaration(String line){
+		try {
+			int numberOfPrefix = this.getTypeAndModifier(line);
+			String[] splitLine = line.trim().split(" ", numberOfPrefix+1);
+//			this is new variable declaration
+//				Pattern pattern = Pattern.compile("[^,;]+? *=* *[^,;]+ *[,;]");
+//				Pattern pattern = Pattern.compile("[,;]");
+//				Matcher matcher = pattern.matcher(splitLine[numberOfPrefix]);
+//				while (matcher.find()) {
+//					splitLine[1].substring(matcher.start(), matcher.end());
+//				}
+			for (String var: splitLine[numberOfPrefix].split("[,;]")) {
+				this.checkVariable(var);
+			}
+		}
+		catch (Exception e) {
+//			Todo
+		}
+	}
+	private boolean checkValue(String value) {
+		return true;
+	}
+	private void checkVariable(String var) throws Exception{
+		String[] nameAndValue = var.split("[^\"] *= *");
+		String variableName = nameAndValue[0];
+		if (nameAndValue.length > 2 || !this.checkName(variableName)
+			||nameAndValue.length == 2 && !this.checkValue(nameAndValue[1])) {
+			throw new Exception("invalid variable name");
+		} else if (nameAndValue.length == 2) {
+//			createVariable(variableName, VariableValue, this.type, this.value);
+		} else {
+//			createVariable(variableName, VariableValue, this.type, ???);
+		}
+
+
+	}
+
+	/**
+	 * checks if there is valid final modifier and/or valid type name.
+	 * @param line line contains variable declaration that ends with ; or ,
+	 * @return 0 if there is no type/ final in the declaration. 1 if there is valid type name, 2 if there are
+	 * both valid final modifier and type name.
+	 */
+	private int getTypeAndModifier(String line) throws Exception{
+		String[] splitLine = line.trim().split(" *\\w+ *[=,;] *.*",2)[0].split(" ");
+		if (splitLine.length == 2 && this.validFinal(splitLine[0]) && this.validType(splitLine[1])) {
+			return splitLine.length;
+		} else if (splitLine.length == 1 && this.validType(splitLine[0])) {
+			return splitLine.length;
+		}
+		else if (splitLine.length==0) {
+			return splitLine.length;
+		}
+		else {
+			throw new Exception("invalid variable declaration");
+		}
+	}
+	private boolean validType(String toCheck) {
+		this.type =toCheck;
+		return true;
+	}
+	private boolean validFinal(String modifier) {
+		return this.isFinal = modifier.equals("final");
+	}
+	public boolean findVariables(Matcher matcher, String[] splitLine) {
+		while (matcher.find()) {
+			splitLine[1].substring(matcher.start(), matcher.end());
+		}
+		return true;
+	}
+//	public boolean checkVariables(String declaration, String type) {
+//		String[] splitDeclaration = declaration.split("=[^\"]",2);
+//		if (this.checkName(splitDeclaration[0]) && splitDeclaration.length>1) {
+//
+//		}
+//	}
 }
